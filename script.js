e520219998ebd9a241f87d406bfd5518a72a237f
Irish Micole Cando <irishmicolecando@gmail.com>
2023-03-13 17:25:43 +0800

    Declare elements to be used

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict'
+
+const inputAmount = document.getElementById("amount");
+const tipBtns = document.querySelectorAll(".tip-btn");
+const customTip = document.getElementById("custom-percent-tip");
+const numberOfPeople = document.getElementById("number-of-people");
+const tipAmount = document.getElementById("tip-amount");
+const totalTip = document.getElementById("amount-per-person");
+const resetBtn = document.getElementById("btn-reset");
